5d59bcf9a56ce258ca827d0d4b4362aa36bc2a8d
abulkassova <131253411+abulkassova@users.noreply.github.com>
2023-11-12 21:11:27 +0100

    Add files via upload

--- WeGotYummy/WeGotYummy/settings.py
+++ WeGotYummy/WeGotYummy/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-vfuk4=msuijtj8+mks1m0@3d0^efs1ow#hy@qi-tda-#v+e#a4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    'clabsql.clamv.jacobs-university.de'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
